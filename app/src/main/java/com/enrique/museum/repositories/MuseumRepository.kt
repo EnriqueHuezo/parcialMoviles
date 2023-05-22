397df237d987f3dcb22c70a10fb04d67b13d1751
Carlos-Portillo <00155321@uca.edu.sv>
2023-05-22 13:39:05 -0600

    :white_check_mark: capa Repository

---
+++ app/src/main/java/com/enrique/museum/repositories/MuseumRepository.kt
@@ -0,0 +1,8 @@
+package com.enrique.museum.repositories
+
+import com.enrique.museum.data.model.MuseumModel
+
+class MuseumRepository(private val museums: MutableList<MuseumModel>) {
+    fun getMuseums() = museums
+    fun addMuseum(museum: MuseumModel) = museums.add(museum)
+}
